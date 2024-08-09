17882302597e658b9f0a8320f771be5eb2fe03c4
Nishant Kumar <nishant.kumar@wisetechglobal.com>
2024-08-09 14:17:23 +0530

    test lists

--- src/test/java/com/nk2/crudapp/CrudAppApplicationTests.java
+++ src/test/java/com/nk2/crudapp/CrudAppApplicationTests.java
@@ -9,5 +9,44 @@ class CrudAppApplicationTests {
     @Test
     void contextLoads() {
     }
+    /*
+    Tests
+        Application creates database and tables exist
+        Employee
+            Create
+                success
+                failure - missing attributes
+                success - when no departments passed
+                failure -  department does not exist
+            Retrieve by id
+                success
+                Exception check for employee not exist
+            Update
+                success
+                failure - employee id does not exist
+                failure - department id does not exist
+            Delete
+                success
+                failure - employee id does not exist
+
+
+        Department
+            Create
+                success
+                failure - missing attributes
+            Retrieve
+                success
+                failure - department id does not exist
+            Update
+                success -
+                failure - readonly department update
+                failure - id does not exist
+            Delete
+                success
+                failure - id does not exist
+                failure - readonly
+
+     */
+
 
 }
